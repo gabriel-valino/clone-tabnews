967703c0fcad07f38ecaf94e2263942fe1caaf15
Gabriel <gabrielvalino25@hotmail.com>
2025-03-17 21:08:24 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,26 +4,30 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  const { updated_at } = responseBody;
-  const { version, max_connections, oppened_connections } =
-    responseBody.dependencies.database;
+      const { updated_at } = responseBody;
+      const { version, max_connections, oppened_connections } =
+        responseBody.dependencies.database;
 
-  expect(updated_at).toBeDefined();
-  const parsedUpdatedAt = new Date(updated_at).toISOString();
-  expect(updated_at).toEqual(parsedUpdatedAt);
+      expect(updated_at).toBeDefined();
+      const parsedUpdatedAt = new Date(updated_at).toISOString();
+      expect(updated_at).toEqual(parsedUpdatedAt);
 
-  expect(version).toBeDefined();
-  expect(version).toEqual("16.0");
+      expect(version).toBeDefined();
+      expect(version).toEqual("16.0");
 
-  expect(version).toBeDefined();
-  expect(max_connections).toEqual(100);
+      expect(version).toBeDefined();
+      expect(max_connections).toEqual(100);
 
-  expect(oppened_connections).toBeDefined();
-  expect(oppened_connections).toEqual(1);
+      expect(oppened_connections).toBeDefined();
+      expect(oppened_connections).toEqual(1);
+    });
+  });
 });
